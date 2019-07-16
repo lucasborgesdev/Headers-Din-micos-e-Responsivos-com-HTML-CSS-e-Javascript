06f30b755c5f422f54362ad628d24caaadf917ba
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2019-07-16 16:19:08 -0300

    Finalização do primeiro header animado
    Finalizado com animação de transição e header dinâmico  e responsivo!

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const $header = document.querySelector("header");
 const $logo = document.querySelectorAll('.logo')[0];
+const $navbar = document.querySelectorAll('.nav-bar')[0];
 window.addEventListener('scroll', toggleHeader, false);
 function toggleHeader(){
     if (window.pageYOffset > 60 && $header.classList.contains("max-header")){
@@ -9,7 +10,8 @@ function toggleHeader(){
         $logo.firstElementChild.setAttribute("src", "imgs/omnitech-logo-2.png");
         $logo.classList.remove('max-logo');
         $logo.classList.add('min-logo');
-       
+        $navbar.classList.remove('max-nav');
+        $navbar.classList.add('min-nav');
     } else if(window.pageYOffset <= 60 && $header.classList.contains("min-header")){
         console.log('HEADER MAIOR!');
         $header.classList.add('max-header');
@@ -17,5 +19,7 @@ function toggleHeader(){
         $logo.firstElementChild.setAttribute("src", "imgs/omnitech-logo-1.png");
         $logo.classList.add('max-logo');
         $logo.classList.remove('min-logo');
+        $navbar.classList.add('max-nav');
+        $navbar.classList.remove('min-nav');
     }
   }
