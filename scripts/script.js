85c79b3e271cf0a347326fd177ca36225ae47d9a
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2019-07-17 19:52:43 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const $header = document.querySelector("header");
 const $logo = document.querySelectorAll('.logo')[0];
 const $navbar = document.querySelectorAll('.nav-bar')[0];
+const $menu = document.querySelectorAll('.menu')[0];
 window.addEventListener('scroll', toggleHeader, false);
 function toggleHeader(){
     if (window.pageYOffset > 60 && $header.classList.contains("max-header")){
@@ -12,6 +13,8 @@ function toggleHeader(){
         $logo.classList.add('min-logo');
         $navbar.classList.remove('max-nav');
         $navbar.classList.add('min-nav');
+        $menu.firstElementChild.classList.remove('max-hamburguer')
+        $menu.firstElementChild.classList.add('min-hamburguer')
     } else if(window.pageYOffset <= 60 && $header.classList.contains("min-header")){
         console.log('HEADER MAIOR!');
         $header.classList.add('max-header');
@@ -21,5 +24,33 @@ function toggleHeader(){
         $logo.classList.remove('min-logo');
         $navbar.classList.add('max-nav');
         $navbar.classList.remove('min-nav');
+        $menu.firstElementChild.classList.add('max-hamburguer')
+        $menu.firstElementChild.classList.remove('min-hamburguer')
     }
+  }  
+  $menu.addEventListener('click', toggleMenu,false);
+  var isOpen = false;
+  // função que verifica se o menu esta aberto ou não somente em  mobile !!! 
+  function toggleMenu(){
+      if(!isOpen){
+        $navbar.classList.add('menu-opened');
+        $menu.firstElementChild.classList.add('close-btn');
+        isOpen = true;
+      } else{
+        $navbar.classList.remove('menu-opened');
+        $menu.firstElementChild.classList.remove('close-btn');
+        isOpen = false;
+      }
+      
   }
+  $navbar.addEventListener('click', navClick, false);
+  // evt de evento
+  function navClick(evt){
+    if (evt.target.tagName == 'A'){
+        toggleMenu();
+    }
+      
+
+  }
+
+  
